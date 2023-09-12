6b3ce733220d6c52531319c2e5d9ef298a587648
Ivo Limić <ilimic1@gmail.com>
2023-09-12 10:42:50 +0200

    adding PUC setup.

--- plugin.php
+++ plugin.php
@@ -10,7 +10,15 @@ Author URI: https://ilimic.from.hr
 
 namespace Ilimic\Plugins\HelloWorld1;
 
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
 require_once __DIR__ . '/vendor/autoload.php';
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://github.com/ilimic1/php-monorepo-wordpress-plugin-1',
+	__FILE__, //Full path to the main plugin file or functions.php.
+	'ilimic/hello-world-1'
+);
 
 function hello_world() {
 	echo '<p>Hello World 1</p>';
