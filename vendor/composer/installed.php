fbaab5efd08b31bd0cbbb94c584d0aa8e93ba2b6
Ivo Limić <ilimic1@gmail.com>
2023-09-14 13:56:08 +0200

    adding vendor dirs to git.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ilimic/hello-world-1',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'da34953182f4ddf12242c6c2dc4149483f67f396',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ilimic/hello-world-1' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'da34953182f4ddf12242c6c2dc4149483f67f396',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yahnis-elsts/plugin-update-checker' => array(
+            'pretty_version' => 'v5.2',
+            'version' => '5.2.0.0',
+            'reference' => '5a270988c5f76bfdfbbb42cccc7c9627f7dd64d0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yahnis-elsts/plugin-update-checker',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
